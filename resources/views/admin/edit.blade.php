a83ff4e1d1dd7ed5ac930f9302fa2c3acf2b5198
Juan Salvatore <juansalvatore@live.com.ar>
2017-11-12 19:35:51 -0300

    Add edit verification

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -7,11 +7,16 @@
                 <div class="form-group">
                     <label for="title">Title</label>
                     <input type="text" class="form-control" id="title" name="title" value="{{ $post['title'] }}">
-                    
+                    @if($errors->first('title'))
+                    <p class="alert alert-danger">{{ $errors->first('title') }}</p>
+                    @endif
                 </div>
                 <div class="form-group">
                     <label for="content">Content</label>
                     <input type="text" class="form-control" id="content" name="content" value="{{ $post['content'] }}">
+                    @if($errors->first('content'))
+                    <p class="alert alert-danger">{{ $errors->first('content') }}</p>
+                    @endif
                 </div>
                 {{ csrf_field() }}
                 <button type="submit" class="btn btn-primary">Submit</button>
